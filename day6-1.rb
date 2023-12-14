72f118467d089282c5237a7c973590e9b11f60a8
Jamie Shiell <jamie@infernus.org>
2023-12-14 21:21:44 +0000

    Day 6-1

---
+++ day6-1.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+races = []
+
+File.open('day6.txt', 'r') do |f|
+    times = []
+    f.readline.split(' ').drop(1).each { |length| times << length.to_i }
+    f.readline.split(' ').drop(1).each do |record| 
+        races << {
+            :time => times.shift,
+            :record => record.to_i
+        }
+    end
+end
+
+options_per_race = []
+races.each do |race|
+    winning_options = 0
+    (1 .. race[:time] - 1).each do |hold|
+        movement_time = race[:time] - hold
+        winning_options += 1 if hold * movement_time > race[:record]
+    end
+    options_per_race << winning_options
+end
+
+puts options_per_race.inspect
+puts options_per_race.inject(:*)
